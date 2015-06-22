f405432a42ac620ebe99653c2b348ae0379b8f68
wq9 <quan.we@gmail.com>
2015-06-21 23:50:55 -0400

    Handle resize, change background, pet

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -25,6 +25,27 @@
     fileref2.setAttribute('type','text/javascript');
     fileref2.setAttribute('src', 'raw.js');
     document.getElementsByTagName('head')[0].appendChild(fileref2);
+    
+    //handle resize
+    function setCanvasSize(){
+      var div = document.getElementById('canvasDiv');
+      var viewport = document.getElementById('canvas');
+      
+      var w = div.clientWidth;
+      var h = div.clientHeight;
+      
+      if(w>h){
+        viewport.style.width=h.toString()+'px';
+        viewport.style.height='';
+      }else{
+        viewport.style.height=w.toString()+'px';
+        viewport.style.width='';
+      }
+    }
+    window.addEventListener('resize', setCanvasSize, true);
+    
+    
+    
   });
   
 })(document);
